1627876d5f51881aab65606b2aa775e7026a0287
Clonephaze <jackdsmith2327@gmail.com>
2023-06-27 16:05:55 -0700

    Update BCML Bug Fix.bat
    added comments for those that care

--- BCML Bug Fix.bat
+++ BCML Bug Fix.bat
@@ -1,13 +1,18 @@
 @echo off
+
+rem Uninstall the bcml package
 echo Uninstalling bcml package...
 pip uninstall bcml --yes >nul
 echo BCML package uninstalled.
 
+rem Remove the merged folder
 rd /s /q %LOCALAPPDATA%\bcml\merged
 
+rem Install the bcml package
 echo Installing bcml package...
 pip install bcml --no-input >nul
 echo bcml package installed.
 
+rem Open BCML
 echo Done! Opening BCML
 bcml
